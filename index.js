09910b922bb353c08c95dc6d8ccb78b647bccf35
AnaMolly <analia.mohamdally@gmail.com>
2021-06-13 16:45:29 -0400

    added extensions to employee class and required them in index.js

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 const inquirer = require("inquirer")
 const fs = require("fs")
+const Manager = require("./lib/Manager")
+const Engineer = require("./lib/Engineer")
+const Intern = require("./lib/Intern")
+
+
+
+function init() {
+
+
+}
+
+init()
