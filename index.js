bbde7e9b9228b8a95d531531fc6c5877e522b806
AnaMolly <analia.mohamdally@gmail.com>
2021-06-13 20:16:41 -0400

    added qinquirer questions and functions to index.js

--- index.js
+++ index.js
@@ -6,8 +6,102 @@ const Intern = require("./lib/Intern")
 
 
 
-function init() {
 
+const managerQuestions = [ { 
+    type: 'input',
+    message: "What is the team Manager's name?",
+    name: 'name',     
+    },
+    { 
+    type: 'input',
+    message: "What is the team Manager's ID?",
+    name: 'id',     
+    },
+    { 
+    type: 'input',
+    message: "What is the team Manager's email address?",
+    name: 'email',     
+    },
+    {
+    type: 'input',
+    message: "What is the team Manager's office number?",
+    name: 'officenum', 
+    } 
+]
+
+const engineerQuestions = [ { 
+    type: 'input',
+    message: "What is the Engineer's name?",
+    name: 'name',     
+    },
+    { 
+    type: 'input',
+    message: "What is the Engineer's ID?",
+    name: 'id',     
+    },
+    { 
+    type: 'input',
+    message: "What is the Engineer's email address?",
+    name: 'email',     
+    },
+    {
+    type: 'input',
+    message: "What is the Engineer's GitHub URL?",
+    name: 'github',  
+    }
+]
+const internQuestions = [
+    { 
+    type: 'input',
+    message: "What is the Intern's name?",
+    name: 'name',     
+    },
+    { 
+    type: 'input',
+    message: "What is the Intern's ID?",
+    name: 'id',     
+    },
+    { 
+    type: 'input',
+    message: "What is the Intern's email address?",
+    name: 'email',     
+    },
+    {
+    type: 'input',
+    message: "Where does the Intern go to school?",
+    name: 'school',  
+    }
+]
+
+const moreMembers = [
+    {
+        type: 'list',
+        message: 'Finish building your team. What role would you like to add next?',
+        name: 'nextrole',
+        choices: ['Engineer', 'Intern', 'Manager',"I am finished building my team."],      
+    }
+]
+
+function initialQuestion(){
+    inquirer.prompt(managerQuestions)
+    .then((managerData)=>{
+        console.log(managerData)
+
+    let addMoreMembers = () => {
+        inquirer.prompt(moreMembers)
+        }
+        addMoreMembers()
+    })
+
+    
+
+    
+}
+
+
+
+function init() {
+    initialQuestion()
 
 }
 
